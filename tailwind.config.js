55fe63c590a4dec54c96e7199c8a8a9c841669b2
Hijodkain <hijodkain@hotmail.com>
2025-08-27 23:07:43 +0200

    ultimos ajustes para que funcione en servidor local. se hace un npm install y luego npm run dev desde la terminal de Visual Studio Code y funciona.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./**/*.{js,ts,jsx,tsx,mdx}",
+    "./*.{tsx,ts}",
   ],
   theme: {
     extend: {},
