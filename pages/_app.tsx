8867de2cf05cf3c52a4604d5e116637fb72d76e8
Hijodkain <hijodkain@hotmail.com>
2025-09-01 16:59:52 +0100

    feat: Add new application logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>PWA M3U Manager</title>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
