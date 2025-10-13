b584d19146d911849e6dad2643ee2ba86cea1c44
Juan carlos <fisiorapid@gmail.com>
2025-10-13 20:43:37 +0200

    Elimina dependencias obsoletas: antd, worker-loader y AntIcon.tsx

--- next.config.js
+++ next.config.js
@@ -1,38 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: [
-    '@ant-design/icons-svg',
-    '@ant-design/icons',
-    'rc-util',
-    'rc-pagination',
-    'rc-picker',
-    'rc-table',
-    'rc-tree',
-    'antd'
-  ],
   webpack: (config, { isServer }) => {
-    // Web Worker support
-    config.module.rules.push({
-      test: /\.worker\.ts$/,
-      loader: 'worker-loader',
-      options: {
-        filename: 'static/[hash].worker.js',
-        publicPath: '/_next/',
-      },
-    });
-
-    // Fix for worker-loader in Next.js
-    if (!isServer) {
-      config.output.globalObject = 'self';
-    }
-
-    // Add support for ant-design icons and rc-util
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@ant-design/icons/lib/dist$': '@ant-design/icons/lib/index.js',
-      'rc-util/es/Dom/canUseDom': 'rc-util/lib/Dom/canUseDom',
-    };
+    // Web Workers are natively supported in Next.js 14+ with new Worker(new URL(...))
+    // No need for worker-loader anymore
 
     // ESM modules fix
     config.resolve.extensionAlias = {
